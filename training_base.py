c25be40e5c54d6e9f03b09161d1310120777dbcf
Songtuan Lin <songtuan.lin@gmail.com>
2025-08-19 23:37:56 +0200

    add another example jani file

--- training_base.py
+++ training_base.py
@@ -3,10 +3,10 @@ from stable_baselines3 import PPO
 from stable_baselines3.common.env_util import make_vec_env
 
 
-model_file = "examples/bouncing_ball.jani"
-start_file = "examples/start_compact.jani"
-goal_file = "examples/objective.jani"
-safe_file = "examples/safe.jani"
+model_file = "examples/inverted_pendulum/inverted_pendulum.jani"
+start_file = "examples/inverted_pendulum/start.jani"
+goal_file = "examples/inverted_pendulum/objective.jani"
+safe_file = "examples/inverted_pendulum/safe.jani"
 
 env = JaniEnv(model_file, start_file, goal_file, safe_file)
 envs = make_vec_env(JaniEnv, n_envs=5, env_kwargs=dict(model_file=model_file, start_file=start_file, goal_file=goal_file, safe_file=safe_file))
