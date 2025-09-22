1ec7f9af84dfdf32523ec43ddd91f674fe5e9d93
Songtuan Lin <songtuanlin@jddhcp48.cs.uni-saarland.de>
2025-09-22 17:45:18 +0200

    fix a warning

--- offline_rl/__init__.py
+++ offline_rl/__init__.py
@@ -3,7 +3,6 @@ Offline Reinforcement Learning (RL) module for JANI environments.
 '''
 
 from .load_dataset import read_trajectories, create_replay_buffer
-from .iql import hyperparameter_tuning, train
 from .loss import DiscreteIQLLossValueLB, DiscreteIQLLossQValueLB
 from .models import create_q_module, create_v_module, create_actor
 
@@ -11,8 +10,6 @@ from .models import create_q_module, create_v_module, create_actor
 __all__ = [
     "read_trajectories", 
     "create_replay_buffer",
-    "hyperparameter_tuning",
-    "train",
     "DiscreteIQLLossValueLB",
     "DiscreteIQLLossQValueLB",
     "create_q_module",
